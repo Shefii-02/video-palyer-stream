85a5dd98cc216338051d7f2e2a664af7f56ca39f
Shefii <shefii.indigital@gmail.com>
2023-04-11 14:20:03 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<video controls width="250" controlsList="nodownload">
+
+    <source src="stream.php"
+            type="video/webm">
+
+    <source src="/stream.php"
+            type="video/mp4">
+</video>
---
+++ stream.php
@@ -0,0 +1,130 @@
+<?php
+
+class PlayVideoStream
+{
+    /** set the private data members **/
+    private $path = "";
+    private $stream = "";
+    private $buffer = 1024000;
+    private $start  = -1;
+    private $end    = -1;
+    private $size   = 0;
+    /** set the private data members **/
+    
+    /** Set the video file path in while creating the object **/
+     function __construct($filePath) 
+    {
+        $this->path = $filePath;
+    }
+     
+    /**
+     * Open the file in read mode
+     */
+    private function open()
+    {
+        if (!($this->stream = fopen($this->path, 'rb'))) {
+            die('Not able to read the file i.e '.$this->path);
+        }
+         
+    }
+     
+    /**
+     * Set  header to serve the video play
+     * Here we are getting the mime type dynamically, for this you must enable php_fileinfo extension
+     */
+    private function setHeader()
+    {
+        ob_get_clean();
+        $result = new finfo();
+        $videoType= $result->file($this->path, FILEINFO_MIME_TYPE);  //Get the correct video type
+        header("Content-Type: ".$videoType);
+        header("Cache-Control: max-age=2592000, public");
+        header("Expires: ".gmdate('D, d M Y H:i:s', time()+2592000) . ' GMT');
+        header("Last-Modified: ".gmdate('D, d M Y H:i:s', @filemtime($this->path)) . ' GMT' );
+        $this->start = 0;
+        $this->size  = filesize($this->path);
+        $this->end   = $this->size - 1;
+        header("Accept-Ranges: 0-".$this->end);
+         
+        if (isset($_SERVER['HTTP_RANGE'])) {
+  
+            $c_start = $this->start;
+            $c_end = $this->end;
+ 
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $this->start-$this->end/$this->size");
+                exit;
+            }
+            if ($range == '-') {
+                $c_start = $this->size - substr($range, 1);
+            }else{
+                $range = explode('-', $range);
+                $c_start = $range[0];
+                 
+                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $c_end;
+            }
+            $c_end = ($c_end > $this->end) ? $this->end : $c_end;
+            if ($c_start > $c_end || $c_start > $this->size - 1 || $c_end >= $this->size) {
+                header('HTTP/1.1 416 Requested Range Not valid');
+                header("Content-Range: bytes $this->start-$this->end/$this->size");
+                exit;
+            }
+            $this->start = $c_start;
+            $this->end = $c_end;
+            $length = $this->end - $this->start + 1;
+            fseek($this->stream, $this->start);
+            header('HTTP/1.1 206 Partial Content');
+            header("Content-Length: ".$length);
+            header("Content-Range: bytes $this->start-$this->end/".$this->size);
+        }
+        else
+        {
+            header("Content-Length: ".$this->size);
+        }  
+         
+    }
+    
+    /**
+     * close curretly opened stream
+     */
+    private function end()
+    {
+        fclose($this->stream);        die;
+    }
+     
+    /**
+     * perform the streaming of calculated range
+     */
+    private function stream()
+    {
+        $i = $this->start;
+        set_time_limit(0);
+        while(!feof($this->stream) && $i <= $this->end) {
+            $bytesToRead = $this->buffer;
+            if(($i+$bytesToRead) > $this->end) {
+                $bytesToRead = $this->end - $i + 1;
+            }
+            $data = fread($this->stream, $bytesToRead);
+            echo $data;
+            flush();
+            $i += $bytesToRead;
+        }
+    }
+     
+    /**
+     * Start streaming video content
+     */
+    function start()
+    {
+        $this->open();
+        $this->setHeader();
+        $this->stream();
+        $this->end();
+    }
+}
+
+$filePath = "movie.mp4";
+$stream = new PlayVideoStream($filePath);
+$stream->start();
